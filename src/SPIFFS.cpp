227291903fe79f13e6e6e3800443bbd41a071440
copercini <evandrocopercini@yahoo.com>
2017-07-06 16:47:47 -0300

    Initial commit

---
+++ src/SPIFFS.cpp
@@ -0,0 +1,35 @@
+/* SPIFFS for Arduino ESP32
+ * Author: Evandro Luis Copercini
+ * Part of this code is copied from or inspired by SD_MMC library of Hristo Gochkov.
+*/
+
+#include "vfs_api.h"
+#include <ctype.h>
+
+extern "C" {
+#include "esp_vfs.h"
+#include "esp_vfs_fat.h"
+#include "spiffs_vfs.h"
+}
+#include <SPIFFS.h>
+
+using namespace fs;
+
+SPIFFSVFS::SPIFFSVFS(FSImplPtr impl) : FS(impl)
+{}
+
+bool SPIFFSVFS::begin(const char * mountpoint)
+{
+	vfs_spiffs_register(mountpoint);
+    _impl->mountpoint(mountpoint);
+	return true;
+}
+
+void SPIFFSVFS::end()
+{
+        spiffs_unmount(1); //1 for unregister too
+        _impl->mountpoint(NULL);
+}
+
+
+SPIFFSVFS SPIFFS = SPIFFSVFS(FSImplPtr(new VFSImpl()));
